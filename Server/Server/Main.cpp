4ca23f72d80624ea297b20ad37506f18798b1dd1
derisan <derisan@naver.com>
2022-06-01 20:27:33 +0900

    pch, Logger

--- Server/Server/Main.cpp
+++ Server/Server/Main.cpp
@@ -1,6 +1,8 @@
+#include "stdafx.h"
+
 #include <iostream>
 
 int main()
 {
-	std::cout << "Hello, world!";
+	spdlog::info("Hello ,world!");
 }
---
+++ Server/Server/stdafx.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#pragma warning(push, 0)
+#include <spdlog/spdlog.h>
+#include <spdlog/fmt/ostr.h>
+#pragma warning(pop)
+
+#pragma comment(lib, "spdlog")
+#pragma comment(lib, "fmt")
+
+#include <DirectXTK12/SimpleMath.h>
+using namespace DirectX::SimpleMath;
