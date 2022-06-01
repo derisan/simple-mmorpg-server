f8e29d2167db2270b42711fbde4548fabe0a4374
derisan <derisan@naver.com>
2022-06-01 20:38:14 +0900

    dll 추가

--- Server/Server/stdafx.h
+++ Server/Server/stdafx.h
@@ -1,12 +1,21 @@
 #pragma once
 
+#pragma comment(lib, "spdlog")
+#pragma comment(lib, "fmt")
+#pragma comment(lib, "ws2_32")
+#pragma comment(lib, "mswsock")
+#pragma comment(lib, "libtcmalloc_minimal")
+#pragma comment(linker, "/include:__tcmalloc")
+
 #pragma warning(push, 0)
 #include <spdlog/spdlog.h>
 #include <spdlog/fmt/ostr.h>
 #pragma warning(pop)
 
-#pragma comment(lib, "spdlog")
-#pragma comment(lib, "fmt")
-
 #include <DirectXTK12/SimpleMath.h>
 using namespace DirectX::SimpleMath;
+
+#include <Windows.h>
+#include <Winsock2.h>
+#include <WS2tcpip.h>
+#include <MSWSock.h>
