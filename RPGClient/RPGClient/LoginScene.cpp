e23d964faa3350240495b7c0ae9be3cffb47cb3f
derisan <derisan@naver.com>
2022-06-14 21:29:43 +0900

    로그인 씬

--- RPGClient/RPGClient/LoginScene.cpp
+++ RPGClient/RPGClient/LoginScene.cpp
@@ -1,6 +1,16 @@
 ﻿#include "stdafx.h"
 #include "LoginScene.h"
 
+constexpr float textBoxWidth = 300.0f;
+constexpr float buttonWidth = 100.0f;
+constexpr int32 maxChars = 19;
+
+void LoginScene::Enter()
+{
+	mBackground = Texture{ U"Assets/Login_Background.png" };
+	mTitle = Texture{ U"Assets/Login_Title.png" };
+}
+
 void LoginScene::Update(const float deltaTime)
 {
 	UNREFERENCED_PARAMETER(deltaTime);
@@ -8,5 +18,37 @@ void LoginScene::Update(const float deltaTime)
 
 void LoginScene::Render()
 {
+	renderBackground();
+	renderUI();
+}
 
+void LoginScene::renderBackground()
+{
+	const auto ScreenSize = Scene::Size();
+	Rect{ 0, 0, ScreenSize.x, ScreenSize.y }(mBackground).draw();
+	Rect{ 20, 150, 760, 67}(mTitle).draw();
+}
+
+void LoginScene::renderUI()
+{
+	const auto ScreenSize = Scene::Size();
+	float xPos = (ScreenSize.x - textBoxWidth) / 2.0f;
+
+	SimpleGUI::Headline(
+		U"계정",
+		Vec2{ xPos + 2, ScreenSize.y - 163.0f });
+
+	SimpleGUI::TextBox(
+		mUserInput,
+		Vec2{ xPos, ScreenSize.y - 125.0f },
+		textBoxWidth,
+		maxChars);
+
+	xPos = (ScreenSize.x - buttonWidth) / 2.0f;
+
+	bool isClicked = SimpleGUI::Button(U"로그인", Vec2{ xPos, ScreenSize.y - 75.0f });
+	if (isClicked)
+	{
+		Print << mUserInput.text;
+	}
 }
--- RPGClient/RPGClient/LoginScene.h
+++ RPGClient/RPGClient/LoginScene.h
@@ -6,8 +6,17 @@ class LoginScene :
     public BaseScene
 {
 public:
+	virtual void Enter() override;
 	virtual void Update(const float deltaTime) override;
 	virtual void Render() override;
-	
+
+private:
+	void renderBackground();
+	void renderUI();
+
+private:
+	TextEditState mUserInput = {};
+	Texture mBackground = {};
+	Texture mTitle = {};
 };
 
